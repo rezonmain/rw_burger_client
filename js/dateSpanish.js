26fc732a552ec1e70d321265e6a342a71cdea7f3
rezonmain <adiazgomez101@gmail.com>
2020-12-28 00:38:17 -0800

    linting

--- js/dateSpanish.js
+++ js/dateSpanish.js
@@ -1,52 +1,51 @@
 export function getFullMonthSpanish(number) {
-    switch (number) {
-        case 0:
-            return 'Enero';
-        case 1:
-            return 'Febrero';
-        case 2:
-            return 'Marzo';
-        case 3:
-            return 'Abril';
-        case 4:
-            return 'Mayo';
-        case 5:
-            return 'Junio';
-        case 6:
-            return 'Julio';
-        case 7:
-            return 'Agosto';
-        case 8:
-            return 'Septiembre';
-        case 9:
-            return 'Octubre';
-        case 10:
-            return 'Noviembre';
-        case 11:
-            return 'Diciembre'
-        default:
-            return null
-
-    }
+  switch (number) {
+    case 0:
+      return "Enero";
+    case 1:
+      return "Febrero";
+    case 2:
+      return "Marzo";
+    case 3:
+      return "Abril";
+    case 4:
+      return "Mayo";
+    case 5:
+      return "Junio";
+    case 6:
+      return "Julio";
+    case 7:
+      return "Agosto";
+    case 8:
+      return "Septiembre";
+    case 9:
+      return "Octubre";
+    case 10:
+      return "Noviembre";
+    case 11:
+      return "Diciembre";
+    default:
+      return null;
+  }
 }
 
 export function getNameDaySpanish(number) {
-    switch (number) {
-        case 1:
-            return 'Lunes';
-        case 2:
-            return 'Martes';
-        case 3:
-            return 'Miercoles';
-        case 4:
-            return 'Jueves';
-        case 5:
-            return 'Viernes';
-        case 6:
-            return 'Sabado';
-        case 7:
-            return 'Domingo';
-        default:
-            return null;
-    }
+  switch (number) {
+    case 1:
+      return "Lunes";
+    case 2:
+      return "Martes";
+    case 3:
+      return "Miercoles";
+    case 4:
+      return "Jueves";
+    case 5:
+      return "Viernes";
+    case 6:
+      return "Sabado";
+    case 0:
+      return "Domingo";
+    default:
+      return null;
+  }
 }
